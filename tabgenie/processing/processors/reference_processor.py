59aa8b84edc9a1aba3110738d80e6477e0d0c825
Zdenek Kasner <kasnezde@fel.cvut.cz>
2023-02-23 21:51:01 +0100

    eRmove old unused processorsaan pipelines

--- tabgenie/processing/processors/reference_processor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from ..processing import Processor
-
-
-class ReferenceProcessor(Processor):
-    def process(self, content):
-        table = content["dataset_obj"].get_table(split=content["split"], table_idx=content["table_idx"])
-        return self.text2html(table.ref)
