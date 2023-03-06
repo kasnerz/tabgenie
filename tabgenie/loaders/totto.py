a16a9250119bd061fa02bccf184c1854ef72a9e1
kategerasimenko <katgerasimenko@gmail.com>
2023-03-06 16:21:04 +0100

    fixed totto read

--- tabgenie/loaders/totto.py
+++ tabgenie/loaders/totto.py
@@ -18,6 +18,49 @@ class ToTTo(HFTabularDataset):
     def get_task_definition(self):
         return "Give a description of the selected table cells."
 
+    @staticmethod
+    def _write_cells(table_obj, entry):
+        taken_cells = set()
+
+        for i, row in enumerate(entry["table"]):
+            col_num = 0
+            while (i, col_num) in taken_cells:
+                c = Cell()
+                c.is_dummy = True
+                table_obj.add_cell(c)
+                col_num += 1
+
+            for j, x in enumerate(row):
+                while (i, col_num) in taken_cells:
+                    c = Cell()
+                    c.is_dummy = True
+                    table_obj.add_cell(c)
+                    col_num += 1
+
+                c = Cell()
+                c.value = x["value"]
+                c.colspan = x["column_span"]
+                c.rowspan = x["row_span"]
+                c.is_highlighted = [i, j] in entry["highlighted_cells"]
+                c.is_col_header = x["is_header"] and i == 0
+                c.is_row_header = x["is_header"] and i != 0
+                table_obj.add_cell(c)
+
+                for cs in range(x["column_span"]):
+                    if cs:
+                        c = Cell()
+                        c.is_dummy = True
+                        table_obj.add_cell(c)
+
+                    for rs in range(x["row_span"]):
+                        taken_cells.add((i + rs, col_num + cs))
+
+                col_num += x["column_span"]
+
+            table_obj.save_row()
+
+        return table_obj
+
     def prepare_table(self, entry):
         t = Table()
         t.props["reference"] = entry["target"]
@@ -34,16 +77,6 @@ class ToTTo(HFTabularDataset):
         t.props["overlap_subset"] = entry["overlap_subset"]
         t.props["url"] = entry["table_webpage_url"]
 
-        for i, row in enumerate(entry["table"]):
-            for j, x in enumerate(row):
-                c = Cell()
-                c.value = x["value"]
-                c.colspan = x["column_span"]
-                c.rowspan = x["row_span"]
-                c.is_highlighted = [i, j] in entry["highlighted_cells"]
-                c.is_col_header = x["is_header"] and i == 0
-                c.is_row_header = x["is_header"] and i != 0
-                t.add_cell(c)
-            t.save_row()
+        t = self._write_cells(t, entry)
 
         return t
