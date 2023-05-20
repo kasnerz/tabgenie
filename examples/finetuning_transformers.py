27ffcbeb87bc7926f1a9e6ee00f176aeb662c1f1
kategerasimenko <katgerasimenko@gmail.com>
2023-05-20 20:46:21 +0200

    add kwarg

--- examples/finetuning_transformers.py
+++ examples/finetuning_transformers.py
@@ -224,7 +224,7 @@ def main(dataset, base_model, epochs, batch_size, ckpt_dir, output_dir):
         score_vals[ckpt_path] = {'score': score, 'preds': preds}
 
     # saving best model and its dev results
-    max_ckpt_path, max_ckpt_results = max(score_vals.items(), lambda x: x[1]['score'])
+    max_ckpt_path, max_ckpt_results = max(score_vals.items(), key=lambda x: x[1]['score'])
     best_ckpt_model = AutoModelForSeq2SeqLM.from_pretrained(max_ckpt_path)
     best_ckpt_model.save(os.path.join(save_dir, "model"))
 
